8c339deccb43a5aa50629c678bed79583ad4b04a
Nathan Casarez <nathanweb.dev@proton.me>
2025-12-19 17:53:09 -0800

    fix

--- services/authService.js
+++ services/authService.js
@@ -9,8 +9,7 @@ export const getDashboardData = async () => {
     // window.location.href = 'https://myharada.netlify.app/login'
     // return
   }
-
+  console.log(response)
   const data = await response.json()
-  console.log(data.user)
   return data.user
 }
