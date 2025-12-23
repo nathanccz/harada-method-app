7bbb8e91b589b47c8946d4a85de4edb2d68c4cb9
Nathan Casarez <nathanweb.dev@proton.me>
2025-12-23 15:00:29 -0800

    changes to overview/list component

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -131,7 +131,7 @@ export default function Overview({
             }
             onMouseLeave={() => setHovered(null)}
           >
-            <li className="p-2 pb-2 text-md opacity-80 font-bold tracking-wide flex gap-2 items-center">
+            <li className="p-2 pb-2 text-md opacity-80 font-bold uppercase tracking-wide flex gap-2 items-center">
               {gridData.gridType === 'project' &&
                 !gridData.completedAt &&
                 !gridData.templateCategory && (
@@ -149,12 +149,19 @@ export default function Overview({
                     {calculateFraction(grid)}
                   </div>
                 )}
-              <span>{grid[4].text}</span>
+              <span>
+                {grid.slot === 'middle-center'
+                  ? 'Main Goal'
+                  : ind < 4
+                  ? `Pillar ${ind + 1}`
+                  : `Pillar ${ind - 1}`}
+              </span>
             </li>
 
             {/* Skip the middle cell since it's in the list title */}
             {[...grid.slice(0, 4), ...grid.slice(5)].map((cell, ind) => (
               <li className="list-row p-2" key={cell.id}>
+                <div>{grid[4].text}</div>
                 <div className="text-xl font-thin opacity-80 tabular-nums">
                   {(ind + 1).toString().padStart(2, '0')}
                 </div>
