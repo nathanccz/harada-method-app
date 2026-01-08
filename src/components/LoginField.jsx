6e51f9a94e2ecd8029e30b527c6c68995b5c0df6
Nathan Casarez <nathanweb.dev@proton.me>
2026-01-08 14:15:52 -0800

    goodle auth url fix

--- src/components/LoginField.jsx
+++ src/components/LoginField.jsx
@@ -15,7 +15,7 @@ export default function LoginField() {
 
   const GOOGLE_AUTH_URL = import.meta.env.DEV
     ? 'http://localhost:3000/auth/google'
-    : 'https://myharada-app-backend.onrender.com'
+    : 'https://myharada-app-backend.onrender.com/auth/google'
 
   const FIREBASE_AUTH_URL = import.meta.env.DEV
     ? 'http://localhost:3000/api/auth/firebase-login'
