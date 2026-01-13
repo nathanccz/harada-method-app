c994eb321dc4fff9b848543527960e5c461c0dae
Nathan Casarez <nathanweb.dev@proton.me>
2026-01-13 13:19:31 -0800

    added new cloudinary url for demo video component

--- src/components/DemoVideo.jsx
+++ src/components/DemoVideo.jsx
@@ -3,7 +3,7 @@ export default function DemoVideo() {
     <div className="inset-0">
       <video
         className="w-full h-full object-cover"
-        src="https://res.cloudinary.com/dw2e6ddjn/video/upload/vc_auto,ac_none/myharada-demo-video-compressed_rtqlej.mp4"
+        src="https://res.cloudinary.com/dw2e6ddjn/video/upload/vc_auto,ac_none/myharada-demo-video-compressed-cut-merged-1768338896650_g42jkb.mp4"
         autoPlay
         muted
         playsInline
