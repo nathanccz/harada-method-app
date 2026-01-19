03da0172e2e93d748f2bc829a91fd833f723f410
Nathan Casarez <nathanweb.dev@proton.me>
2026-01-18 17:00:03 -0800

    added new demo video url to homepage

--- src/components/DemoVideo.jsx
+++ src/components/DemoVideo.jsx
@@ -3,7 +3,7 @@ export default function DemoVideo() {
     <div className="inset-0">
       <video
         className="w-full h-full object-cover"
-        src="https://res.cloudinary.com/dw2e6ddjn/video/upload/vc_auto,ac_none/myharada-demo-video-compressed-cut-merged-1768338896650_g42jkb.mp4"
+        src="https://res.cloudinary.com/dw2e6ddjn/video/upload/vc_auto,ac_none/v1768784294/myharada-demo-20260118-compressed_njoxwj.mp4"
         autoPlay
         muted
         playsInline
